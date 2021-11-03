1790cd0463da51a07b9c3abf440a063f5a63fc75
pitrack <paxia@jhu.edu>
2021-11-02 21:13:50 -0400

    remove or move emnlp2020 files

--- conversion_scripts/convert_tf_to_pt.sh
+++
@@ -1,3 +0,0 @@
-echo  $data_dir/$1
-transformers-cli convert --model_type bert --tf_checkpoint $data_dir/$1/model.max.ckpt --config $data_dir/$1/bert_config.json --pytorch_dump_output $data_dir/$1/pytorch_model.bin
-python $2/convert_tf_to_pytorch.py $data_dir/$1/model.max.ckpt $data_dir/$1/torch_scorer_vars.bin
